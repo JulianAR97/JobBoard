2306e1be181e20b4ac225b23f23f0a951335d874
Julian <julianrosenthal@yahoo.com>
2021-02-01 16:22:00 -0700

    generated resource for listings

---
+++ db/migrate/20210201232141_create_listings.rb
@@ -0,0 +1,13 @@
+class CreateListings < ActiveRecord::Migration[6.1]
+  def change
+    create_table :listings do |t|
+      t.string :title
+      t.string :content
+      t.string :difficulty
+      t.integer :price
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
