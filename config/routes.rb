a0c71e3bbe1f2e6634487b49a1c376c226ae639b
Julian <julianrosenthal@yahoo.com>
2021-02-02 10:02:29 -0700

    nested routes for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  resources :listings
+  devise_for :users 
+  
+  # Technically correct, but fix these routes
+  resources :users, except: [:index, :show, :edit, :update, :destroy, :new, :create] do
+    resources :listings
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
