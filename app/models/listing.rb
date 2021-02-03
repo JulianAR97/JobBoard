4d1600f67842f8f8d2e36da2053b29d8cc801b49
Julian <julianrosenthal@yahoo.com>
2021-02-02 17:35:29 -0700

    fixed validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,7 @@ class Listing < ApplicationRecord
   belongs_to :user
   
   validates :title, length: {in: 5..50}
-  validates :difficulty, inclusion: { in: %w(easy medium hard), message: "%{value} is not a valid size" }
+  validates :difficulty, inclusion: { in: %w(easy medium hard), message: "%{value} is not a valid difficulty" }
   validates :price, numericality: { only_integer: true }
   validates :content, length: { in: 100..1000 }
   def creator_name
