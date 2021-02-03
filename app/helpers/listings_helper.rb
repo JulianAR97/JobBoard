bfa79324c27bc344fb5f9d79d76dceb2397689df
Julian <julianrosenthal@yahoo.com>
2021-02-03 14:52:31 -0700

    added a helper method for generating links

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -1,2 +1,9 @@
 module ListingsHelper
+  def show_listing(listing)
+    if listing.user == current_user
+      link_to listing.title, user_listing_path(current_user, listing)
+    else
+      link_to listing.title, listing_path(listing)
+    end
+  end
 end
