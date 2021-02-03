e0db1bd7d594022a766dd27a0eda6ca4ea059ce8
Julian <julianrosenthal@yahoo.com>
2021-02-03 14:55:04 -0700

    added conversion of float to integer before validation for listing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,17 +3,25 @@ class Listing < ApplicationRecord
   has_many :listing_tags
   has_many :tags, through: :listing_tags
 
-  validates :title, length: {in: 5..50}
+  before_validation :price_to_integer
+
+  validates :title, length: { in: 5..50 }
   validates :difficulty, inclusion: { in: %w(easy medium hard), message: "%{value} is not a valid difficulty" }
   validates :price, numericality: { only_integer: true }
   validates :content, length: { in: 100..5000 }
-  
+
   def creator_name
     user.email
   end
 
   def tag_names
     tags.map(&:name)
+  end
+
+  private
+
+  def price_to_integer
+    self.price = self.price.to_i
   end
 end
 
