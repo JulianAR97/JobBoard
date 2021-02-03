57233f5743c68ad371f021d151df3351aa7e2964
Julian <julianrosenthal@yahoo.com>
2021-02-02 17:08:31 -0700

    added validations to listing model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,7 @@
 class Listing < ApplicationRecord
   belongs_to :user
   
+  validates :title, length: {in: 5..50}
   validates :difficulty, inclusion: { in: %w(easy medium hard), message: "%{value} is not a valid size" }
   validates :price, numericality: { only_integer: true }
   validates :content, length: { in: 100..1000 }
@@ -11,3 +12,4 @@ class Listing < ApplicationRecord
 
 
 end
+
