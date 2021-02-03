6305e8a4e0df414a620b4cc23d5861c3970f3bda
Julian <julianrosenthal@yahoo.com>
2021-02-03 14:44:47 -0700

    added generic listing show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
   
   # scope in this instance is the same as saying resources :users except: *methods
   # listings need to be nested under a user to perform crud actions
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   end
 
   # allow all users to look at the listings regardless of session status
-  resources :listings, only: :index
+  resources :listings, only: %i[index show]
 
   root 'static#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
