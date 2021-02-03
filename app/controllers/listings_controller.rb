c1c522cd812f1adc7d987526b94410878957ec8d
Julian <julianrosenthal@yahoo.com>
2021-02-02 17:52:38 -0700

    added crud controller actions to listings_controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,40 @@
 class ListingsController < ApplicationController
   before_action :match_profile
+  before_action :get_listing, only: %i[show edit update destroy]
   def index
     params[:user_id] ? @listings = Listing.where(user_id: params[:user_id]) : @listings = Listing.all
+  end
+  
+  def show 
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create 
+    @listing = current_user.listings.build(listing_params)
+    if @listing.save
+      redirect_to user_listing_path(current_user, @listing), notice: "Listing was successfully created"
+    else 
+      render 'listings/new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    @listing.update(listing_params)
+    if @listing.save 
+      redirect_to user_listing_path(current_user, @listing), notice: "Listing was successfully updated"
+    else
+      render 'listings/edit'
+    end
+  end
+
+  def destroy
+
   end
 
   private
@@ -18,5 +51,13 @@ class ListingsController < ApplicationController
         end
       end
     end
+  end
+
+  def get_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(%i[title content difficulty price])
   end
 end
