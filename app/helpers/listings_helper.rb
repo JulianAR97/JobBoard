5673ab14cea7b28067902d7264bba8598dc917a0
Julian <julianrosenthal@yahoo.com>
2021-02-04 22:01:17 -0700

    added multiple helper methods to clean up listing views

--- app/helpers/listings_helper.rb
+++ app/helpers/listings_helper.rb
@@ -10,4 +10,21 @@ module ListingsHelper
   def valid_skill_levels
     %w[beginner intermediate expert]
   end
+
+  def check_box_options
+    [:tag_ids, Tag.order(:name), :id, :name, limit: 5]
+  end
+
+  def radio_button_options
+    [:skill_level, valid_skill_levels, :to_s, :to_s, {}, required: true]
+  end
+
+  def partial_path(path)
+    "listings/form_partials/#{path}"
+  end
+
+  def custom_collect_class(cllt_type, resource)
+    cllt_type == 'check_box' ? get_tag_class(resource.object.name) : resource.text
+  end
+
 end
