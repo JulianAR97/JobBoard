9ff22b1328d19a374f7e6d7ac208638e585f9a12
Julian <julianrosenthal@yahoo.com>
2021-02-21 23:26:56 -0700

    added to gitignore

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -2,7 +2,7 @@ class JobApplicationMailer < ApplicationMailer
 
   def new_job_application_email
     @job_application = params[:job_application]
-
+   
     mail(to: @job_application.listing.user.email, subject: "Someone Applied to Your Listing")
   end
 end
