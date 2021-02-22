afad0e5ff89a551269ad43b9750b04eeb9abc68d
Julian <julianrosenthal@yahoo.com>
2021-02-22 03:45:18 -0700

    fixed mailer to include attachments

--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -2,7 +2,8 @@ class JobApplicationMailer < ApplicationMailer
 
   def new_job_application_email
     @job_application = params[:job_application]
-   
+    file = open(@job_application.resume.url).read
+    attachments[@job_application.resume] = file
     mail(to: @job_application.listing.user.email, subject: "Someone Applied to Your Listing")
   end
 end
