9e439710be2d5180eda46cdcd3d7302d9d28aef7
Julian <julianrosenthal@yahoo.com>
2021-02-21 20:58:32 -0700

    removed whitespace

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_183350) do
+ActiveRecord::Schema.define(version: 2021_02_18_231749) do
+
+  create_table "job_applications", force: :cascade do |t|
+    t.integer "applicant_id"
+    t.integer "listing_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "listing_tags", force: :cascade do |t|
     t.integer "listing_id"
