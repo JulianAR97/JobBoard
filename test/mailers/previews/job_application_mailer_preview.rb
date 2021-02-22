dc0cea26b9f35d86686d1b7ed07e3214f26e798a
Julian <julianrosenthal@yahoo.com>
2021-02-21 23:14:06 -0700

    added test mailer

---
+++ test/mailers/previews/job_application_mailer_preview.rb
@@ -0,0 +1,6 @@
+class JobApplicationMailerPreview < ActionMailer::Preview
+  # def new_job_application_email
+  #   job_application = JobApplication.new(applicant_id: 1, listing_id: Listing.find_by(user_id: 21).id)
+  #   JobApplicationMailer.with(job_application: job_application).new_job_application_email
+  # end
+end
