1ddedc90630122ac2df789ce77d1243e2aac3202
Julian <julianrosenthal@yahoo.com>
2021-02-21 21:53:50 -0700

    added framework for creating job applications

--- app/controllers/job_application_controller.rb
+++ app/controllers/job_application_controller.rb
@@ -1,10 +1,22 @@
 class JobApplicationController < ApplicationController
 
   def create
+    listing = Listing.find(params[:listing_id])
+
+    @job_application = JobApplication.new(applicant: current_user, listing: listing)
     binding.pry
-    @job_application = JobApplication.new(job_application_params)
+
+    if @job_application.save
+      # do stuff
+      # Also validate that a person cannot apply twice
+      redirect_to listing_path(listing), notice: "You have successfully applied for this job"
+    end
   end
 
 
+  private
 
+  def job_application_params
+    params.permit(:listing_id)
+  end
 end
