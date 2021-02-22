525ad071be88ef67fafb9f4b7feda2e6daca9d24
Julian <julianrosenthal@yahoo.com>
2021-02-21 23:20:58 -0700

    job applications send emails on create

--- app/controllers/job_application_controller.rb
+++ app/controllers/job_application_controller.rb
@@ -4,10 +4,10 @@ class JobApplicationController < ApplicationController
     listing = Listing.find(params[:listing_id])
 
     @job_application = JobApplication.new(applicant: current_user, listing: listing)
-    binding.pry
-
+    
     if @job_application.save
-      # do stuff
+      JobApplicationMailer.with(job_application: @job_application).new_job_application_email.deliver_later
+
       # Also validate that a person cannot apply twice
       redirect_to listing_path(listing), notice: "You have successfully applied for this job"
     end
