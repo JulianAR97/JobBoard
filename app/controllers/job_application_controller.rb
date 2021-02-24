c1514f82e06c585b3724edfa25e528fc42e8b8b7
Julian <julianrosenthal@yahoo.com>
2021-02-23 17:59:42 -0700

    working attachment with user upload resumes

--- app/controllers/job_application_controller.rb
+++ app/controllers/job_application_controller.rb
@@ -1,16 +1,19 @@
 class JobApplicationController < ApplicationController
-
   def create
     listing = Listing.find(params[:listing_id])
  
     @job_application = JobApplication.new(applicant: current_user, listing: listing)
-    @job_application.resume = params[:job_application][:resume]
+    @job_application.resume.attach = job_application_params[:resume]
+    
     if @job_application.save
-
-      JobApplicationMailer.with(job_application: @job_application).new_job_application_email.deliver
+      file = File.read(job_application_params[:resume].tempfile)
+      filename = job_application_params[:resume].original_filename
+      JobApplicationMailer.with(job_application: @job_application).new_job_application_email(file, filename).deliver
 
       # Also validate that a person cannot apply twice
       redirect_to listing_path(listing), notice: "You have successfully applied for this job"
+    else
+      redirect_to listing_path(listing)
     end
   end
 
@@ -18,6 +21,7 @@ class JobApplicationController < ApplicationController
   private
 
   def job_application_params
-    params.permit([:job_application, :listing_id])
+    params.require(:job_application).permit('resume')
   end
+
 end
--- app/mailers/job_application_mailer.rb
+++ app/mailers/job_application_mailer.rb
@@ -1,12 +1,12 @@
 class JobApplicationMailer < ApplicationMailer
 
-  def new_job_application_email
+  def new_job_application_email(file, filename)
     @job_application = params[:job_application]
     
     # file = open(@job_application.resume.url).read
 
     # use actual name in attachments
-    attachments['test_resume.docx'] = File.read('test_resume.docx')
+    attachments[filename] = file
     mail(to: @job_application.listing.user.email, subject: "Someone Applied to Your Listing")
   end
 end
