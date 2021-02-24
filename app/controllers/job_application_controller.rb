3e1e691c701a1f7310204bdff581f294928c0013
Julian <julianrosenthal@yahoo.com>
2021-02-23 19:24:31 -0700

    added file validations for uploads

--- app/controllers/job_application_controller.rb
+++ app/controllers/job_application_controller.rb
@@ -3,14 +3,12 @@ class JobApplicationController < ApplicationController
     listing = Listing.find(params[:listing_id])
  
     @job_application = JobApplication.new(applicant: current_user, listing: listing)
-    
-    
-    if @job_application.save
-      # First we read the tempfile that we get from user upload and save it in a variable
-      file = File.read(job_application_params[:resume].tempfile)
+    # First we read the tempfile that we get from user upload and save it in a variable
+    file = File.read(job_application_params[:resume].tempfile)
+    # Next we grab the filename to use with action mailer
+    filename = job_application_params[:resume].original_filename
 
-      # Next we grab the filename to use with action mailer
-      filename = job_application_params[:resume].original_filename
+    if valid_file?(filename) && @job_application.save
       JobApplicationMailer.with(job_application: @job_application).new_job_application_email(file, filename).deliver
 
       # Also validate that a person cannot apply twice
@@ -22,6 +20,10 @@ class JobApplicationController < ApplicationController
 
 
   private
+  
+  def valid_file?(filename) 
+    filename.end_with?('pdf', 'docx')
+  end
 
   def job_application_params
     params.require(:job_application).permit('resume')
