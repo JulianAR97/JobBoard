53e87df1acd6c405e84074a33541dbebe7b0c9bf
Julian <julianrosenthal@yahoo.com>
2021-02-23 18:06:47 -0700

    cleaned up code

--- app/controllers/job_application_controller.rb
+++ app/controllers/job_application_controller.rb
@@ -3,10 +3,13 @@ class JobApplicationController < ApplicationController
     listing = Listing.find(params[:listing_id])
  
     @job_application = JobApplication.new(applicant: current_user, listing: listing)
-    @job_application.resume.attach = job_application_params[:resume]
+    
     
     if @job_application.save
+      # First we read the tempfile that we get from user upload and save it in a variable
       file = File.read(job_application_params[:resume].tempfile)
+
+      # Next we grab the filename to use with action mailer
       filename = job_application_params[:resume].original_filename
       JobApplicationMailer.with(job_application: @job_application).new_job_application_email(file, filename).deliver
 
